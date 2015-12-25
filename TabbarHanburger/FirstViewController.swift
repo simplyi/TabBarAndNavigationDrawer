b9bba323cbcc9a8d60745c91bd07f97501aeb746
Sergey Kargopolov <flylet@softlets.com>
2015-12-25 14:46:26 -0800

    Initial Commit

---
+++ TabbarHanburger/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabbarHanburger
+//
+//  Created by Sergey Kargopolov on 2015-12-25.
+//  Copyright © 2015 Sergey Kargopolov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
